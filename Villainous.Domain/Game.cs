f420fc58190da19e473f57e2ba129e2452271147
nick <s114623@ap.be>
2022-07-08 23:15:32 +0200

    Added 2 new classes to the domain

---
+++ Villainous.Domain/Game.cs
@@ -0,0 +1,8 @@
+﻿namespace Villainous.Domain;
+
+public class Game
+{
+    public int Id { get; set; }
+    public string Code{ get; set; }
+    public virtual ICollection<Player> Players { get; set; }
+}
---
+++ Villainous.Domain/Player.cs
@@ -0,0 +1,8 @@
+﻿namespace Villainous.Domain;
+
+public class Player
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public virtual Game Game { get; set; }
+}
