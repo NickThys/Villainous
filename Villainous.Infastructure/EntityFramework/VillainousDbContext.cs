60cc33f59d437cfd69e785e4a6f8ded632a4044c
nick <s114623@ap.be>
2022-07-08 14:38:22 +0200

    Added the infastructure

---
+++ Villainous.Infastructure/EntityFramework/VillainousDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Villainous.Infastructure.EntityFramework;
+
+public class VillainousDbContext:DbContext
+{
+    public VillainousDbContext(DbContextOptions options) : base(options)
+    {
+
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=villainous;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        optionsBuilder.UseSqlServer(connString);
+    }
+}
---
+++ Villainous.Infastructure/StartupExtention.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Villainous.Infastructure.EntityFramework;
+
+namespace Villainous.Infastructure;
+
+public static class StartupExtention
+{
+    public static IServiceCollection ConfigureInfastructure(this IServiceCollection services, string connectionString)
+    {
+        services.AddDbContext<VillainousDbContext>(options => options.UseSqlServer(connectionString));
+        return services;
+    }
+}
