6726a9ad3eb8e16eb400ea8efc3009b9ad400d9a
nick <s114623@ap.be>
2022-07-08 14:26:53 +0200

    Added Database Project

---
+++ Villainous.DataAccess/Migrations/M001_Initial.cs
@@ -0,0 +1,31 @@
+﻿using FluentMigrator;
+
+namespace Villainous.DataAccess.Migrations
+{
+    [Migration(1)]
+    public class M001_Initial : Migration
+    {
+        private readonly bool _seed;
+
+        public M001_Initial(Options options)
+        {
+            _seed = options.Seed;
+        }
+
+        public override void Up()
+        {
+            Create.Table("Players")
+                .WithColumn("Id").AsInt32().Identity().PrimaryKey().NotNullable()
+                .WithColumn("Name").AsString().NotNullable()
+                .WithColumn("Position").AsInt32().NotNullable()
+                .WithColumn("IsReady").AsBoolean().NotNullable();
+
+        }
+
+        public override void Down()
+        {
+            Delete.Table("Players");
+        }
+    }
+
+}
--- Villainous.DataAccess/Program.cs
+++ Villainous.DataAccess/Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CommandLine;
+using FluentMigrator.Runner;
+using Microsoft.Extensions.DependencyInjection;
+using Villainous.DataAccess;
+using Villainous.DataAccess.Migrations;
+
+string connectionString = null;
+var options = new Options();
+Parser.Default.ParseArguments<Options>(args).WithParsed(opts =>
+{
+    connectionString = opts.ConnectionString;
+    options = opts;
+}).WithNotParsed((errs) => throw new ArgumentException(string.Join(Environment.NewLine, errs)));
+var serviceProvider = CreateServices(connectionString, options);
+
+using (var scope = serviceProvider.CreateScope())
+{
+    UpdateDatabase(scope.ServiceProvider);
+}
+
+static IServiceProvider CreateServices(string connectionString, Options options)
+{
+    return new ServiceCollection()
+    .AddFluentMigratorCore()
+    .ConfigureRunner(rb => rb
+        .AddSqlServer()
+        .WithGlobalConnectionString(connectionString)
+        .ScanIn(typeof(M001_Initial).Assembly).For.Migrations())
+    .AddLogging(lb => lb.AddFluentMigratorConsole())
+    .AddSingleton(options)
+    .BuildServiceProvider(false);
+}
+
+static void UpdateDatabase(IServiceProvider serviceProvider)
+{
+    var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+    var options = serviceProvider.GetRequiredService<Options>();
+    if (options.Reset)
+    {
+        Retry(() => runner.MigrateDown(0), 5);
+    }
+    runner.MigrateUp();
+}
+
+static void Retry(Action action, int count)
+{
+    for (var i = 0; i < count; i++)
+    {
+        try
+        {
+            action();
+            break;
+        }
+        catch (Exception)
+        {
+            if (i == count)
+            {
+                throw;
+            }
+        }
+    }
+}
