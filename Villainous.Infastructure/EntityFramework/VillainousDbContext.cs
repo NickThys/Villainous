2a32274d3b7172eae665aeeffb1a242a345b356a
nick <s114623@ap.be>
2022-07-08 23:21:05 +0200

    Added the 2 classes to the DbContext

--- Villainous.Infastructure/EntityFramework/VillainousDbContext.cs
+++ Villainous.Infastructure/EntityFramework/VillainousDbContext.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Villainous.Domain;
 
 namespace Villainous.Infastructure.EntityFramework;
 
 public class VillainousDbContext:DbContext
 {
+    public DbSet<Player> Players { get; set; }
+    public DbSet<Game> Games { get; set; }
     public VillainousDbContext(DbContextOptions options) : base(options)
     {
 
